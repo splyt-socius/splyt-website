1cb66b42b85d959400d7486e5787c866216fb241
Joseph Rex <joerexko@gmail.com>
2020-04-02 22:27:20 -0500

    Add sections and images

--- gulpfile.js
+++ gulpfile.js
@@ -12,11 +12,13 @@ const paths = {
     styles: [
       'src/css/**/*.css',
       'src/scss/**/*.scss'
-    ]
+    ],
+    images: 'assets/images/**/*.jpg'
   },
   target: {
     scripts: 'dist/js',
     styles: 'dist/css',
+    images: 'dist/img',
     sourcemaps: './maps',
   }
 };
@@ -46,15 +48,39 @@ function scripts() {
     .pipe(browserSync.stream())
 }
 
+function webP(config) {
+  return src(paths.source.images)
+    .pipe($.webp(config))
+    .pipe(dest(paths.target.images))
+}
+
+function webPFallback(config) {
+  return src(paths.source.images)
+    .pipe($.imagemin([
+      $.imagemin.mozjpeg(config)
+    ]))
+    .pipe(dest(paths.target.images))
+}
+
+function images() {
+  const config = {
+    quality: 75
+  };
+  return webP(config)
+    .pipe(webPFallback(config));
+}
+
 function serve() {
   browserSync.init({ server: { baseDir: './' } });
   watch('src/scss/**/*.scss', series('styles'));
   watch('src/js/*.js', series('scripts'));
+  watch('assets/images/**/*.jpg', series('images'));
   watch('*.html').on('change', browserSync.reload);
 }
 
 exports.styles = styles;
 exports.scripts = scripts;
+exports.images = images;
 exports.serve = serve;
 exports.build = parallel(styles, scripts);
-exports.default = parallel(styles, scripts, serve);
+exports.default = parallel(styles, scripts, images, serve);
